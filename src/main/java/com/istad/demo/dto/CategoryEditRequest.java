79355512d69cd614b2d547e2e392cb660716f92b
unknown <taingey2966@gmail.com>
2024-03-23 21:25:32 +0700

    Project MVC new Updated

--- src/main/java/com/istad/demo/dto/CategoryEditRequest.java
+++
@@ -1,7 +0,0 @@
-package com.istad.demo.dto;
-
-public record CategoryEditRequest(
-        String name,
-        String description
-) {
-}
---
+++ src/main/java/com/istad/demo/dto/CategoryRequest.java
@@ -0,0 +1,2 @@
+package com.istad.demo.dto;public record CategoryRequest() {
+}
